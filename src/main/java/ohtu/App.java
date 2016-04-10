dda18882f6bdddf2f2a23bb5f7a598fd21b5a9ee
Outi Jussila <outi.jussila@helsinki.fi>
2016-04-10 23:38:18 +0300

    lopussa

--- src/main/java/ohtu/App.java
+++ src/main/java/ohtu/App.java
@@ -5,6 +5,8 @@ import ohtu.data_access.UserDao;
 import ohtu.io.ConsoleIO;
 import ohtu.io.IO;
 import ohtu.services.AuthenticationService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
 
@@ -23,47 +25,65 @@ public class App {
         return userPwd;
     }
 
-    public void run() {
-        while (true) {
-            String command = io.readLine(">");
+    private boolean onko() {
+        String command = io.readLine(">");
 
-            if (command.isEmpty()) {
-                break;
-            }
-
-            if (command.equals("new")) {
-                String[] usernameAndPasword = ask();
-                if (auth.createUser(usernameAndPasword[0], usernameAndPasword[1])) {
-                    io.print("new user registered");
-                } else {
-                    io.print("new user not registered");
-                }
-
-            } else if (command.equals("login")) {
-                String[] usernameAndPasword = ask();
-                if (auth.logIn(usernameAndPasword[0], usernameAndPasword[1])) {
-                    io.print("logged in");
-                } else {
-                    io.print("wrong username or password");
-                }
-            }
-
+        if (command.isEmpty()) {
+            return false;
+        } else {
+            kasky(command);
+            return true;
         }
     }
 
-    public static void main(String[] args) {
-        UserDao dao = new InMemoryUserDao();
-        IO io = new ConsoleIO();
-        AuthenticationService auth = new AuthenticationService(dao);
-        new App(io, auth).run();
+    private void kasky(String command) {
+        if (command.equals("new")) {
+            uusi();
+        } else if (command.equals("login")) {
+            kirjaudu();
+        }
     }
-    
+
+    private void uusi() {
+        String[] usernameAndPasword = ask();
+        if (auth.createUser(usernameAndPasword[0], usernameAndPasword[1])) {
+            io.print("new user registered");
+        } else {
+            io.print("new user not registered");
+        }
+    }
+
+    private void kirjaudu() {
+        String[] usernameAndPasword = ask();
+        if (auth.logIn(usernameAndPasword[0], usernameAndPasword[1])) {
+            io.print("logged in");
+        } else {
+            io.print("wrong username or password");
+        }
+    }
+
+    public void run() {
+        while (onko());
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("src/main/resources/spring-context.xml");
+
+        App application = ctx.getBean(App.class
+        );
+        application.run();
+//        UserDao dao = new InMemoryUserDao();
+//        IO io = new ConsoleIO();
+//        AuthenticationService auth = new AuthenticationService(dao);
+//        new App(io, auth).run();
+    }
+
     // testejä debugatessa saattaa olla hyödyllistä testata ohjelman ajamista
-    // samoin kuin testi tekee, eli injektoimalla käyttäjän syötteen StubIO:n avulla
-    //
-    // UserDao dao = new InMemoryUserDao();  
-    // StubIO io = new StubIO("new", "eero", "sala1nen" );   
-    //  AuthenticationService auth = new AuthenticationService(dao);
-    // new App(io, auth).run();
-    // System.out.println(io.getPrints());
+// samoin kuin testi tekee, eli injektoimalla käyttäjän syötteen StubIO:n avulla
+//
+// UserDao dao = new InMemoryUserDao();  
+// StubIO io = new StubIO("new", "eero", "sala1nen" );   
+//  AuthenticationService auth = new AuthenticationService(dao);
+// new App(io, auth).run();
+// System.out.println(io.getPrints());
 }
